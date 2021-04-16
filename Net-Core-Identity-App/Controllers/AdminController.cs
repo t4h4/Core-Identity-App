0f7fe3c0832f425117bbd0228fa31bfa12ef34b6
t4h4 <tahayasin@outlook.com>
2021-04-17 01:51:05 +0300

    UserManager DI ile AdminController'a eklendi.

---
+++ Net-Core-Identity-App/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Net_Core_Identity_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Net_Core_Identity_App.Controllers
+{
+    public class AdminController : Controller
+    {
+        private UserManager<AppUser> userManager { get; }
+        // DI sayesinde UserManager'dan nesne alabilir ve onun içini doldurabiliriz. 
+        public AdminController(UserManager<AppUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View(userManager.Users.ToList());
+        }
+    }
+}
