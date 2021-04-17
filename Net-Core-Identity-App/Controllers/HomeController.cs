666249f35a4168ccb7315ea6cb9bdb3af6b7ce21
t4h4 <tahayasin@outlook.com>
2021-04-17 06:42:07 +0300

    SignUp sayfası eklendi.

--- Net-Core-Identity-App/Controllers/HomeController.cs
+++ Net-Core-Identity-App/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Net_Core_Identity_App.Models;
+using Net_Core_Identity_App.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -12,10 +14,12 @@ namespace Net_Core_Identity_App.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        public UserManager<AppUser> userManager { get; } // UserManager DI islemi yapiyoruz. 
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager)
         {
             _logger = logger;
+            this.userManager = userManager;
         }
 
         public IActionResult Index()
@@ -33,5 +37,50 @@ namespace Net_Core_Identity_App.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
+
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(UserViewModel userViewModel)
+        {
+            if(ModelState.IsValid) // view'de girilen model validasyonu gecerliyse, dogruysa
+            {
+                AppUser user = new AppUser();
+                user.UserName = userViewModel.UserName;
+                user.Email = userViewModel.Email;
+                user.PhoneNumber = userViewModel.PhoneNumber;
+
+                // IdentityResult result olarak degiskene atiyoruz ki daha fazla detaylara bu degisken sayesinde ulasabilelim.
+                IdentityResult result = await userManager.CreateAsync(user, userViewModel.Password); // pass hash islemi yapilacagi icin burda. 
+
+                if(result.Succeeded) // eger kullanici basarili sekilde kaydolduysa onu login ekranina yonlendirecegiz. 
+                {
+                    return RedirectToAction("LogIn");
+                }
+                else
+                {
+                    foreach (IdentityError item in result.Errors)
+                    {
+                        ModelState.AddModelError("", item.Description);
+                    }
+                }
+            }
+
+
+            return View(userViewModel); // icine userViewModel yazdik cunku kullanici hatali olursa textbox'lari tekrar doldurmayla ugrasmasin hazir gelsin diye.
+        }
+
     }
 }
---
+++ Net-Core-Identity-App/ViewModels/UserViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Net_Core_Identity_App.ViewModels
+{
+    public class UserViewModel
+    {
+        [Required(ErrorMessage = "Kullanıcı ismi gerekldir.")]
+        [Display(Name = "Kullanıcı Adı")]
+        public string UserName { get; set; }
+
+        [Display(Name = "Tel No:")]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Email adresi gereklidir.")]
+        [Display(Name = "Email Adresiniz")]
+        [EmailAddress(ErrorMessage = "Email adresiniz doğru formatta değil")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Şifreniz gereklidir.")]
+        [Display(Name = "Şifre")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
