5164d61324e2a1561c222ba67c49e8100035adb0
t4h4 <tahayasin@outlook.com>
2021-04-18 20:52:45 +0300

    Login sayfası tasarlandı, signup sayfası düzenlendi.

--- Net-Core-Identity-App/Startup.cs
+++ Net-Core-Identity-App/Startup.cs
@@ -42,7 +42,6 @@ namespace Net_Core_Identity_App
 
             cookieBuilder.Name = "MyBlog";
             cookieBuilder.HttpOnly = false; // client side cookie bilgisi okunmayacak. sadece http istegi uzerinden cookie bilgisini almak istiyorum.
-            cookieBuilder.Expiration = System.TimeSpan.FromDays(60); // 60 gun boyunca cookie bilgisini tutacak. 
             cookieBuilder.SameSite = SameSiteMode.Lax; // siteler arasi cookie paylasimi acik. alt domain'de ayný cookie yapisini kullanabilir. strict olsaydi kisitli olurdu. detaylar icin anahtar Cross-Site Request Forgery (CSRF)
             cookieBuilder.SecurePolicy = CookieSecurePolicy.SameAsRequest; // always olsaydý sadece https istegiyle cookie sunucuya giderdi. SameAsRequest hangisinden gelirse ondan gonderiyor http ya da https farketmez. ilk http'den kaydedildiyse sadece http'den gonderir. ama none olursa bu da onemsiz olur. ne olursa olsun gonderir. 
 
@@ -51,6 +50,7 @@ namespace Net_Core_Identity_App
                 opts.LoginPath = new PathString("/Home/Login");
                 opts.Cookie = cookieBuilder;
                 opts.SlidingExpiration = true; // cookie omrunun yarisi gectikten sonra kullanici tekrar istek yaparsa default olarak sure tekrar 60 gune cikarilir. 
+                opts.ExpireTimeSpan = System.TimeSpan.FromDays(60); // 60 gun boyunca cookie bilgisini tutacak. 
             });
 
 
---
+++ Net-Core-Identity-App/ViewModels/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Net_Core_Identity_App.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Display(Name = "Email")]
+        [Required(ErrorMessage = "Email alanı gereklidir")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Display(Name = "Şifre")]
+        [Required(ErrorMessage = "Şifre alanı gereklidir")]
+        [DataType(DataType.Password)]
+        [MinLength(4, ErrorMessage = "Şifreniz en az 4 karakterli olmalıdır.")]
+        public string Password { get; set; }
+    }
+}
