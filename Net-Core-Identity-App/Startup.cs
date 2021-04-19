264260c0f5f08224b5e1387becb007cbb03e3c7d
t4h4 <tahayasin@outlook.com>
2021-04-19 22:58:47 +0300

    Member sayfası yetkisiz girişe karşı korundu.

--- Net-Core-Identity-App/Startup.cs
+++ Net-Core-Identity-App/Startup.cs
@@ -71,7 +71,7 @@ namespace Net_Core_Identity_App
 
             services.ConfigureApplicationCookie(opts =>
             {
-                opts.LoginPath = new PathString("/Home/Login");
+                opts.LoginPath = new PathString("/Home/Login"); // yetki olmayan sayfalara girince buraya gidecek. 
                 opts.Cookie = cookieBuilder;
                 opts.SlidingExpiration = true; // cookie omrunun yarisi gectikten sonra kullanici tekrar istek yaparsa default olarak sure tekrar 60 gune cikarilir. 
                 opts.ExpireTimeSpan = System.TimeSpan.FromDays(60); // 60 gun boyunca cookie bilgisini tutacak. 
