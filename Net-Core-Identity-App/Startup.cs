c37dc3279ff9cba1ec1ede55c47c1e5e2970d53a
t4h4 <tahayasin@outlook.com>
2021-06-02 00:14:50 +0300

    Connection string edited.

--- Net-Core-Identity-App/Startup.cs
+++ Net-Core-Identity-App/Startup.cs
@@ -35,8 +35,8 @@ namespace Net_Core_Identity_App
             // entity servis ekleme
             services.AddDbContext<AppIdentityDbContext>(opts =>
             {
-                // opts.UseSqlServer(Configuration["ConnectionStrings:DefaultConnectionString"]);
-                opts.UseSqlServer(Configuration["ConnectionStrings:DefaultAzureConnectionString"]);
+                opts.UseSqlServer(Configuration["Default:ConnectionString"]);
+                // opts.UseSqlServer(Configuration["DefaultAzure:ConnectionString"]);
             });
 
 
